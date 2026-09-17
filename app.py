dc49bc7a7ee1af6bfd8866af19b3d327eb83fd07
Vitor <victorrs2609@gmail.com>
2026-09-17 14:22:32 -0300

    Corrige inicializacao do banco

--- app.py
+++ app.py
@@ -325,12 +325,7 @@ def excluir(gasto_id):
 # INICIAR SISTEMA
 # ---------------------------------------------------
 
+init_db()
+
 if __name__ == "__main__":
-
-    init_db()
-
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
